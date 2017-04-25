801473b4e862ddfcf0d2ed0bc00278ac64ac2beb
roman_bregey <bregey1@gmail.com>
2017-04-25 19:40:32 +0300

    add files

---
+++ scripts/video_script.js
@@ -0,0 +1,4 @@
+$('document').ready(function() {
+    var options = {videoId: 'N5K8BaV-0GE', start: 1};
+    $('#wrapper').tubular(options);
+});
---
+++ scripts/weather_script.js
@@ -0,0 +1,25 @@
+$(function() {
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError); 
+});
+
+function locationSuccess(position) {
+var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?lat='+position.coords.latitude+
+'&lon='+position.coords.longitude+'&units=metric&APPID=7d50e3a92cc3853d1849b06543304cd1&callback=?';
+$.getJSON(weatherAPI, function(response) {
+console.log(response);
+$('#loc').html(response.city.name);
+$('#country').html(response.country)
+
+$('#temp').html(Math.round(response.list[0].main.temp));
+$('#humidity').html(response.list[0].main.humidity)
+$('#pressure').html(Math.round(response.list[0].main.pressure))
+$('#description').html(response.list[0].weather[0].description)
+
+});
+}
+
+
+
+function locationError(error) {
+console.warn('Error:' + error.message);
+}
